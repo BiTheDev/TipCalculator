0d02f84c129fb7beb16d68e76a59ba21eea8b233
BrianHo <Curry30@Brians-MacBook-Pro.local>
2020-10-05 14:32:44 -0700

    detailed bill tip  display

--- main.js
+++ main.js
@@ -13,11 +13,12 @@ function calculatePrice(){
     let ppl = document.getElementById('sharingPpl').value
     total = bill * tip;
     document.getElementById('price').style.display = 'block';
-    document.getElementById('total').innerHTML = total;
-    
+    document.getElementById('total').innerHTML = total.toFixed(2);
+    document.getElementById('finalTip').innerHTML = total.toFixed(2) - bill;
     if(ppl > 0){
         document.getElementById('splitPrice').style.display = 'block';
         document.getElementById('splitAmt').innerHTML = (total / ppl).toFixed(2);
+        document.getElementById('splitTip').innerHTML = ((total.toFixed(2) - bill) / ppl).toFixed(2);
     }else{
         document.getElementById('splitPrice').style.display = 'none';
     }
